230b5c2906c3654a96d26a6dc2cd1896c482daf7
Bill Cui <bill.cui.work@gmail.com>
2024-03-20 17:04:56 -0400

    part a

--- a3/part-A/partA_connect.sh
+++ a3/part-A/partA_connect.sh
@@ -18,7 +18,7 @@ done
 # Avoid having to write "-O OpenFlow13" before all of your ovs-ofctl commands.
 ofctl='ovs-ofctl -O OpenFlow13'
 
-# OVS rules for switch 1
+# OVS rules for switch h1->h4
 $ofctl add-flow s1 \
     in_port=1,ip,nw_src=10.0.1.2,nw_dst=10.0.4.2,actions=mod_dl_src:0A:00:0C:01:00:03,mod_dl_dst:0A:00:0D:01:00:03,output=3
 
@@ -30,6 +30,21 @@ $ofctl add-flow s3 \
 
 $ofctl add-flow s4 \
     in_port=2,ip,nw_src=10.0.1.2,nw_dst=10.0.4.2,actions=mod_dl_src:0A:00:04:01:00:01,mod_dl_dst:0A:00:04:02:00:00,output=1
+
+# OVS rules for switch h4->h1
+$ofctl add-flow s4 \
+    in_port=1,ip,nw_src=10.0.4.2,nw_dst=10.0.1.2,actions=mod_dl_src:0A:00:0E:FE:00:02,mod_dl_dst:0A:00:0E:01:00:03,output=2
+
+$ofctl add-flow s3 \
+    in_port=3,ip,nw_src=10.0.4.2,nw_dst=10.0.1.2,actions=mod_dl_src:0A:00:0D:FE:00:02,mod_dl_dst:0A:00:0C:FE:00:04,output=2
+
+$ofctl add-flow s2 \
+    in_port=4,ip,nw_src=10.0.4.2,nw_dst=10.0.1.2,actions=mod_dl_src:0A:00:0D:01:00:03,mod_dl_dst:0A:00:0C:01:00:03,output=3
+
+$ofctl add-flow s1 \
+    in_port=3,ip,nw_src=10.0.4.2,nw_dst=10.0.1.2,actions=mod_dl_src:0A:00:01:01:00:01,mod_dl_dst:0A:00:01:02:00:00,output=1
+
+
 
 
 # Print the flows installed in each switch
